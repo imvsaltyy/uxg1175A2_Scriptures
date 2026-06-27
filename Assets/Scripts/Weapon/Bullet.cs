c1a4cf635fdb94a4bdc183f8c7ba1f8fd1e56259
zaborkia <cassandracheongxy@gmail.com>
2026-06-27 22:34:20 +0800

    Added some sound
    Audio manager added
    Button click script
    -character bullet dmg sound
    -main menu bgm
    -button click sound

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -5,41 +5,42 @@ public class Bullet : MonoBehaviour
     public float bulletSpeed = 1f;
     public string fireTag = "";
     public float damageDelt;
+    private AudioManager audioManager;
 
     private void Start()
     {
         Destroy(gameObject, 5);
-
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
 
     private void FixedUpdate()
     {
         transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Check to ensure it is not friendly fire
+        // Check to ensure it is not friendly fire
         if (fireTag != collision.tag)
         {
-            //Debug.Log("Dealing Damage");
+            // Destroy bullet on hit
             Destroy(gameObject);
 
             if (collision.tag == "Player")
             {
-                //
+                // Deal damage
                 collision.gameObject.GetComponent<PlayerStats>().baseHP -= damageDelt;
-            }
 
+                // Play alienshoot sound
+                if (audioManager != null)
+                {
+                    audioManager.PlaySFX(audioManager.alienshoot);
+                }
+            }
             else if (collision.tag == "Enemy")
             {
                 collision.gameObject.GetComponent<spawnEnemy>().HP -= damageDelt;
-                //Debug.Log("Dealing Damage");
-
             }
-
         }
-
     }
 }
